cdbfea0844f9f9d18c8cd39cb1415b41812e55c3
AK <ak@thecarousell.com>
2018-03-24 09:46:25 +0800

    More tests.

--- es2csv_cli/export.py
+++ es2csv_cli/export.py
@@ -1,64 +1,78 @@
 import os
 import itertools
 
-from tqdm import tqdm
+import tqdm
 import elasticsearch
 from .utils import csv_util
 
 
 def _extract_to_csv(data, write_headers, fieldnames, output_folder,
-                    scroll_id):
+                    worker_id):
     for key, group in itertools.groupby(data, key=lambda x: x['_index']):
         _rows = [_datumn['_source'] for _datumn in list(group)]
-        output_file = _output_file_for(output_folder, scroll_id, key)
+        output_file = _output_file_for(output_folder, worker_id, key)
         csv_util.write_dicts_to_csv(
             _rows, output_file, write_headers=write_headers,
             fieldnames=fieldnames[key])
 
 
-def _output_file_for(output_folder, scroll_id, index):
-    return os.path.join(output_folder, '{}_{}.csv'.format(index, scroll_id))
+def _output_file_for(output_folder, worker_id, index):
+    return os.path.join(output_folder, '{}_{}.csv'.format(index, worker_id))
 
 
 def _get_es_client(es_hosts):
     return elasticsearch.Elasticsearch(es_hosts)
 
 
-def scroll_and_extract_data(scroll_id, total_worker_count, es_hosts,
-                            es_timeout, search_args, fieldnames,
-                            output_folder, progress_bar=True):
-    _es = _get_es_client(es_hosts)
-    # Init scroll
+def _add_slice_if_needed(total_worker_count, search_args, worker_id):
     if total_worker_count > 1:
         # Add ES scroll slicing to handle parallel scrolling of the same data
         search_args['body']['slice'] = {
-            'id': scroll_id,
+            'id': worker_id,
             'max': total_worker_count
         }
-    _page = _es.search(**search_args)
+    return search_args
+
+
+def _get_data_from_es_page(_page):
     _data = _page['hits']['hits']
     _sid = _page['_scroll_id']
+    return _data, _sid
+
+
+def _init_progressbar(total_hits, worker_id):
+    return tqdm.tqdm(total=total_hits, position=worker_id,
+                     desc="slice-%s" % worker_id, ascii=True)
+
+
+def scroll_and_extract_data(worker_id, total_worker_count, es_hosts,
+                            es_timeout, search_args, fieldnames,
+                            output_folder, progress_bar=True):
+    _es = _get_es_client(es_hosts)
+    search_args = _add_slice_if_needed(
+        total_worker_count, search_args, worker_id)
+    _page = _es.search(**search_args)
+    _data, _sid = _get_data_from_es_page(_page)
+    _total_hits = _page['hits']['total']
     _headers_written = False
     pbar = None
     if progress_bar:
-        pbar = tqdm(total=_page['hits']['total'], position=scroll_id,
-                    desc="slice-%s" % scroll_id, ascii=True)
+        pbar = _init_progressbar(_total_hits, worker_id)
     if _data:
         _extract_to_csv(data=_data, output_folder=output_folder,
                         write_headers=(not _headers_written),
-                        fieldnames=fieldnames, scroll_id=scroll_id)
+                        fieldnames=fieldnames, worker_id=worker_id)
         _headers_written = True
         if pbar:
             pbar.update(len(_data))
     while True:
-        _page = _es.scroll(scroll_id=_sid, scroll='{}m'.format(es_timeout))
-        _sid = _page['_scroll_id']
-        _data = _page['hits']['hits']
+        _page = _es.scroll(worker_id=_sid, scroll='{}m'.format(es_timeout))
+        _data, _sid = _get_data_from_es_page(_page)
         if _data:
             _extract_to_csv(data=_data,
                             output_folder=output_folder,
                             write_headers=(not _headers_written),
-                            fieldnames=fieldnames, scroll_id=scroll_id)
+                            fieldnames=fieldnames, worker_id=worker_id)
             _headers_written = True
             if pbar:
                 pbar.update(len(_data))
--- tests/test_export.py
+++ tests/test_export.py
@@ -1,12 +1,18 @@
+import tqdm
 import mock
 import unittest
 
+from _pytest.monkeypatch import MonkeyPatch
 from es2csv_cli.export import (
-    extract_to_csv, scroll_and_extract_data, get_fieldnames_for
+    _extract_to_csv, scroll_and_extract_data, get_fieldnames_for,
+    _add_slice_if_needed, _init_progressbar
 )
 
 
 class TestFileUtil(unittest.TestCase):
+    def setUp(self):
+        self.monkeypatch = MonkeyPatch()
+
     def test_extract_to_csv(self):
         data = [
             {
@@ -34,8 +40,8 @@ class TestFileUtil(unittest.TestCase):
         _csv_util = mock.MagicMock()
         with mock.patch('es2csv_cli.utils.csv_util.write_dicts_to_csv',
                         _csv_util):
-            extract_to_csv(data, write_headers=True, fieldnames=_fieldnames,
-                           output_folder=_output_folder, scroll_id=0)
+            _extract_to_csv(data, write_headers=True, fieldnames=_fieldnames,
+                            output_folder=_output_folder, worker_id=0)
             _expected_rows = [
                 {'name': 'n1', 'value': 2}, {'name': 'n2', 'value': 2}]
             _expected_output_folder = _output_folder + '/index1_0.csv'
@@ -79,16 +85,16 @@ class TestFileUtil(unittest.TestCase):
         _es_mock.scroll.side_effect = [_page, _empty_page]
         _es_connect_mock = mock.MagicMock(return_value=_es_mock)
         _csv_util = mock.MagicMock()
+        self.monkeypatch.setattr(tqdm, 'tqdm', mock.MagicMock())
         with mock.patch('elasticsearch.Elasticsearch', _es_connect_mock):
-            with mock.patch('es2csv_cli.export.extract_to_csv', _csv_util):
+            with mock.patch('es2csv_cli.export._extract_to_csv', _csv_util):
                 scroll_and_extract_data(
                     _scroll_id, _total_worker_count, _es_hosts, _timeout,
-                    _search_args, _fieldnames, _output_folder,
-                    progress_bar=False)
+                    _search_args, _fieldnames, _output_folder)
                 _es_connect_mock.assert_called_with(_es_hosts)
                 _es_mock.search.assert_called_with(
                     body={'slice': {'max': 2, 'id': 0}})
-                _es_mock.scroll.assert_called_with(scroll_id=10, scroll='1m')
+                _es_mock.scroll.assert_called_with(worker_id=10, scroll='1m')
 
     def test_get_fieldnames_for(self):
         _es_hosts = 'es_hosts'
@@ -103,3 +109,22 @@ class TestFileUtil(unittest.TestCase):
         with mock.patch('elasticsearch.Elasticsearch', _es_connect_mock):
             fieldnames = get_fieldnames_for(_es_hosts, _indices)
             assert fieldnames['index1'] == ['name']
+
+    def test_no_slice_for_single_worker(self):
+        _search_args = dict(body={})
+        _res = _add_slice_if_needed(1, _search_args, 0)
+        assert _res['body'] == {}
+
+    def test_add_slice_config_for_multiple_worker(self):
+        _search_args = dict(body={})
+        _res = _add_slice_if_needed(2, _search_args, 0)
+        assert _res['body'] == {'slice': {'id': 0, 'max': 2}}
+        _res = _add_slice_if_needed(2, _search_args, 1)
+        assert _res['body'] == {'slice': {'id': 1, 'max': 2}}
+
+    def test_init_progressbar(self):
+        _tqdm = mock.MagicMock()
+        with mock.patch('tqdm.tqdm', _tqdm):
+            _init_progressbar(100, 0)
+            _tqdm.assert_called_with(
+                position=0, total=100, ascii=True, desc='slice-0')
